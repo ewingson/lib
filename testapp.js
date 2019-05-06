6f6fe4fcf5233175ae558ef16b5d1c77a6307b20
Matthias Evering <36561948+ewingson@users.noreply.github.com>
2019-05-06 12:34:14 +0200

    Update testapp.js

--- testapp.js
+++ testapp.js
@@ -3,4 +3,7 @@ console.log("start of code");
 
 const $rdf = require(‘rdflib’);
 const store1  = $rdf.graph();
+  
+const me = store1.sym('https://testpro.solidweb.org/profile/card#me');
+const profile = me.doc();
 }
