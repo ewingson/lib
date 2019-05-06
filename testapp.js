e118942c0a673fb0612a685521248c9d1c1c677f
Matthias Evering <36561948+ewingson@users.noreply.github.com>
2019-05-06 13:09:13 +0200

    Update testapp.js

--- testapp.js
+++ testapp.js
@@ -1,14 +1,14 @@
 function execCode() {
 console.log("start of code");
 
-const $rdf = require(‘rdflib’);
+const $rdf = require('rdflib');
 const store1  = $rdf.graph();
   
 const me = store1.sym('https://testpro.solidweb.org/profile/card#me');
 const profile = me.doc();
   
-const VCARD = new $rdf.Namespace(‘http://www.w3.org/2006/vcard/ns#‘);
-store1.add(me, VCARD(‘fn’), “Matthias configedit Evering”, profile);
+const VCARD = new $rdf.Namespace('http://www.w3.org/2006/vcard/ns#');
+store1.add(me, VCARD('fn'), "Matthias configedit Evering", profile);
   
 let name = store1.any(me, VCARD('name'), null, profile);
 let role = store1.any(me, VCARD('role'), null, profile);
