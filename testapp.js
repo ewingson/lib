e7d149e38ca9c132ea16d9b7366a781746ec224a
Matthias Evering <36561948+ewingson@users.noreply.github.com>
2019-05-10 12:44:28 +0200

    Update testapp.js

--- testapp.js
+++ testapp.js
@@ -28,5 +28,5 @@ console.log(role);
 console.log(org);
 //further things
 //we' re in a queue at provider whether making rdflib in scripts accessible with the booked product is possible or not
-//the dedicated machine (which would enable it) is tied to the prototype server instance
+//the dedicated machine (which would enable it) is tied to the prototype server instance and cannot be used because of the reverse proxy configuration
 }
