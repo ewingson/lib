9a9dbc3351f7f4b2ae7b91e2757991bbbba537f8
Matthias Evering <36561948+ewingson@users.noreply.github.com>
2019-05-12 11:50:35 +0200

    Update index.js

--- 6/index.js
+++ 6/index.js
@@ -1,10 +1,14 @@
+//dependency - express application generator
 const express = require('express')
+//initialize
 const app = express();
-
+//request - resource
 app.get('/', (req, res) => {
+//string
   res.send('Hello World!')
 });
-
+//listen on port 8000
 app.listen(8000, () => {
+//message
   console.log('Example app listening on port 8000!')
 });
