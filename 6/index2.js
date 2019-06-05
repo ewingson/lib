2027b3bcbcb27035117131e54c275ea37e429b0a
Matthias Evering <36561948+ewingson@users.noreply.github.com>
2019-06-05 02:50:47 +0200

    Update index2.js

--- 6/index2.js
+++ 6/index2.js
@@ -2,20 +2,17 @@
 var express = require('express');
 //initialize
 var app = express();
-
 //request - resource
 app.get('/', function (req, res) {
 //string
    res.send('Hello World');
 })
-
 //create server
 var server = app.listen(8000, function () {
 //assign adress
    var host = server.address().address
 //assign port
    var port = server.address().port
-   
 //log      
    console.log("Example app listening at http://%s:%s", host, port)
 })
