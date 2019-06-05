d8c0d3d10aa607a7b658efe331e0e097e6248948
Matthias Evering <36561948+ewingson@users.noreply.github.com>
2019-06-05 02:49:03 +0200

    Create index2.js

---
+++ 6/index2.js
@@ -0,0 +1,21 @@
+//dependency
+var express = require('express');
+//initialize
+var app = express();
+
+//request - resource
+app.get('/', function (req, res) {
+//string
+   res.send('Hello World');
+})
+
+//create server
+var server = app.listen(8000, function () {
+//assign adress
+   var host = server.address().address
+   //assign port
+   var port = server.address().port
+   
+//log      
+   console.log("Example app listening at http://%s:%s", host, port)
+})
