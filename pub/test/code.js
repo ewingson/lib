5aa2f73a815c4af8180d926dc8a659010de1d64d
Matthias Evering <36561948+ewingson@users.noreply.github.com>
2020-02-10 23:19:05 +0100

    indentation / readability
    d2020-02-10 t23:18

--- pub/test/code.js
+++ pub/test/code.js
@@ -1,76 +1,89 @@
 //to identify links in a given doc and add them to markbook
-function openFile(callBack){
-	var element = document.createElement('input');
-	element.setAttribute('type', "file");
-  	element.setAttribute('id', "btnOpenFile");
-  	element.onchange = function(){
-    	readText(this,callBack);
-    	document.body.removeChild(this);
+function openFile(callBack)
+{
+    var element = document.createElement('input');
+    element.setAttribute('type', "file");
+    element.setAttribute('id', "btnOpenFile");
+    element.onchange = function()
+    {
+        readText(this,callBack);
+        document.body.removeChild(this);
     }//end function onchange (L6)
 
-  	element.style.display = 'none';
-  	document.body.appendChild(element);
+    element.style.display = 'none';
+    document.body.appendChild(element);
 
-  	element.click();
+    element.click();
 }//end function openFile (L2)
 
-function readText(filePath,callBack) {
+function readText(filePath,callBack)
+{
     var reader;
-    if (window.File && window.FileReader && window.FileList && window.Blob) {
+    if (window.File && window.FileReader && window.FileList && window.Blob)
+    {
         reader = new FileReader();
-    } else {
+    }
+    else
+    {
         alert('file APIs not supported. case fallback.');
         return false;
     }//end else
     var output = ""; //placeholder for text output
-    if(filePath.files && filePath.files[0]) {           
-        reader.onload = function (e) {
+    if(filePath.files && filePath.files[0])
+    {           
+        reader.onload = function (e)
+        {
             output = e.target.result;
             callBack(output);
         };//end function onload() (L27)
         reader.readAsText(filePath.files[0]);
     }//end if html5 filelist support
-    else { //this is where you could fallback to Java Applet, Flash or similar
+    else
+    { //this is where you could fallback to Java Applet, Flash or similar
         return false;
     }//end else      
     return true;
 }//end function readText (L17)
 
-function execCode() {
+function execCode()
+{
     console.log ("start of code");
-    document.getElementById('btnOpen').onclick = function(){
-        openFile(function(txt){
-			hit = new Array();
+    document.getElementById('btnOpen').onclick = function()
+    {
+        openFile(function(txt)
+        {
+            hit = new Array();
             document.getElementById('tbMain').value = txt;
-	    	//console.log (txt);
-	    	var textArea = document.getElementById('tbMain');
-	    	var line = textArea.value.split("\n");
-        	for(var i = 0;i < line.length-1;i++){
-            	console.log (i+":"+line[i]+line[i].length);
+            //console.log (txt);
+            var textArea = document.getElementById('tbMain');
+            var line = textArea.value.split("\n");
+            for(var i = 0;i < line.length-1;i++)
+            {
+                console.log (i+":"+line[i]+line[i].length);
 
-				for (var j=0; j<line[i].length; j++)
-					{
-					//search for "<a href = \'"
-					var expression=line[i].substr(j,11);
-					if (expression == "<a href = \'") 
-						{
-						//console.log("hit");
-						var anfang = j+11;
-						for (var k=anfang; k<line[i].length; k++)
-							{
-							if (line[i].substr(k,1) == "\'")
-								{
-								//console.log ("the end");
-								var ende = k;
-								hit[i] = 1;
-								console.log("true:"+line[i].substr(anfang, ende-anfang));
-								}//end if k
-							else { hit[i] = 0; }
-							}//end for k
-						}//end if expression
+                for (var j=0; j<line[i].length; j++)
+                {
+                    //search for "<a href = \'"
+                    var expression=line[i].substr(j,11);
+                    if (expression == "<a href = \'") 
+                    {
+                        //console.log("hit");
+                        var anfang = j+11;
+                        for (var k=anfang; k<line[i].length; k++)
+                        {
+                            if (line[i].substr(k,1) == "\'")
+                            {
+                                //console.log ("the end");
+                                var ende = k;
+                                hit[i] = 1;
+                                console.log("true:"+line[i].substr(anfang, ende-anfang));
+                            }//end if k
+                            else { hit[i] = 0; }
+                        }//end for k
+                    }//end if expression
 				
-					}//end for j
-			}//end for i
+                }//end for j
+            }//end for i
        	});//end function openFile (L43)
     }//end function onclick (L42)
     
