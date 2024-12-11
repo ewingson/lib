b41d6ff0177da1bbda0195e51f2c71c774dd0299
Matthias Evering <me@evering.eu>
2024-12-11 23:12:13 +0100

    bootstrap

--- exp/test1.js
+++ exp/test1.js
@@ -1,2 +1,22 @@
+//paypal-experiment
+"use strict";
 $type = "Switch";
-$three_d_secure_version = "0.9.5"
+$three_d_secure_version = "0.9.5";
+path = '/var/aes-nonce';
+async function main(id) {
+  // Function-level strict mode syntax
+  "use strict";
+  console.log('outer function called');
+  function second() {
+    console.log('inner function called');
+    const trust = "teststring";
+    return trust;
+  }
+  return `usual return ${second()}`;
+}
+const var0 = true;
+console.log('start_of_code');
+main(var0);
+//this is completely nonsense, I just try to pass a var
+const $var = "init";
+console.log($var);
